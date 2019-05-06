0a25bcb32aa1029569af0b32b7226b0b3b88d955
Dr. Safi <sfmissive@gmail.com>
2019-05-07 02:34:42 +0300

    build

--- source/script.js
+++ source/script.js
@@ -1,34 +1,51 @@
-import filesystem from 'fs'
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.listContent = void 0;
+
+var _fs = _interopRequireDefault(require("fs"));
 
 // get direcotry contents list
-export const listContent = ({
-  dir, // single path or array of directory paths.
+const listContent = ({
+  dir,
+  // single path or array of directory paths.
   filelist = [],
-  option,
+  option
 } = {}) => {
-  if (!Array.isArray(dir)) dir = [dir] // in case a single string, convert it to array to work with the function.
+  if (!Array.isArray(dir)) dir = [dir]; // in case a single string, convert it to array to work with the function.
 
   for (let directoryPath of dir) {
-    filelist = filelist.concat(listContentSingleContent({ directoryPath, option }))
+    filelist = filelist.concat(listContentSingleContent({
+      directoryPath,
+      option
+    }));
   }
 
-  return filelist
-}
+  return filelist;
+};
+
+exports.listContent = listContent;
 
 function listContentSingleContent({
   directoryPath,
   filelist = [],
   option = {
-    recursive: false,
-  },
+    recursive: false
+  }
 }) {
-  if (!filesystem.existsSync(directoryPath)) return filelist
-  filesystem.readdirSync(directoryPath).forEach(content => {
+  if (!_fs.default.existsSync(directoryPath)) return filelist;
+
+  _fs.default.readdirSync(directoryPath).forEach(content => {
     if (option.recursive) {
-      filelist = filesystem.statSync(path.join(directoryPath, content)).isDirectory() ? listContent(path.join(directoryPath, content), filelist) : filelist.push(content)
+      filelist = _fs.default.statSync(path.join(directoryPath, content)).isDirectory() ? listContent(path.join(directoryPath, content), filelist) : filelist.push(content);
     } else {
-      filelist.push(content)
+      filelist.push(content);
     }
-  })
-  return filelist
+  });
+
+  return filelist;
 }
